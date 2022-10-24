95f84e29e03212448bbea9bcf0a8263518f5c100
kazoo1122 <kazoo1122@experienced.work>
2022-10-24 11:07:21 +0900

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Kazumasa Ohira on 2022/10/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
